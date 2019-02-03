7e460c793ed46b1451c490cb382b07ac4fbedd1b
DESKTOP-9RG54CS\Daffa Mage <daffaabiyyum@gmail.com>
2019-02-03 11:15:54 +0700

    no message

---
+++ index.php
@@ -0,0 +1,20 @@
+<HTML>
+	<head>
+		<title>Warcraft 3 Utilities</title>
+	</head>
+	
+	<style>
+	/* Body of HTML */
+	body {
+		background-color: #6699FF;
+	}
+	</style>
+
+	<body>
+		<a href="w3armor/w3armor.php">Warcraft 3 Armor</a>
+		<br>
+		<br>
+		<b></b><size=30><a href="https://github.com/DaffaMage/w3scripts">GET THE WARCRAFT 3 SCRIPTS</a></size></b>
+	</body>
+	
+</HTML>
